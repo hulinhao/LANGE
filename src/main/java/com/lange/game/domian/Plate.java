c80c0f1e52406c71de2559434f0b8bff84679c83
hulinhao <hulinhao@beadwallet.com>
2020-06-24 10:07:44 +0800

    修改plate表

--- src/main/java/com/lange/game/domian/Plate.java
+++ src/main/java/com/lange/game/domian/Plate.java
@@ -16,7 +16,8 @@ public class Plate {
     private Long gameId;//比赛id
     private String content;//盘口内容
     private BigDecimal odds;//赔率
-    private Integer type;//0:可下注 1:盘口过期 3：已赔付
+    private int type;       // 1:game   2:project
+    private int status; //0:可下注 1:盘口过期 3：已赔付
     private Date createTime;//
     private Date endTime;//
     private Date paidTime;//赔付时间
